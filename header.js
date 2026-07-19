6e3e62a93e1697506d89802c0459603e5b590c69
ernst <ernst.n.641@gmail.com>
2026-07-19 22:41:35 +0200

    Enforce no horizontal scrolling

--- header.js
+++ header.js
@@ -4,17 +4,6 @@ fetch('header.html')
     .then(data => {
         document.getElementById('header-container').innerHTML = data;
 
-        // get active page and header items
-        const activePage = window.location.pathname.split('/').pop() || 'index.html';
-        const headerItems = document.querySelectorAll('.header-item');
-
-        // loop through header items and apply append active style to active page
-        headerItems.forEach(item => {
-            if (item.getAttribute('href') === activePage) {
-                item.classList.add('active');
-            }
-        });
-
         const contactBtn = document.querySelector('.header-item:last-child');
         contactBtn.style.cursor = 'pointer';
         contactBtn.addEventListener('click', () => {
